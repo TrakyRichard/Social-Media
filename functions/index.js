653f5dac1320b47f199af1149177ab004587ec9c
kanmadozo <trakyrichardkanm3@gmail.com>
2020-06-30 16:05:05 +0100

    V2

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,35 +9,228 @@ admin.initializeApp();
 // });
 
 exports.onCreateFollower = functions.firestore
-    .document("/followers/{userId}/userFollowers/{followerId}")
-     .onCreate(async (snapshot, context) => {
-     console.log("Follower Created", snapshot.data());
-        const userId = context.params.userId;
-        const followerId = context.params.followerId;
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onCreate(async (snapshot, context) => {
+    console.log("Follower Created", snapshot.id);
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
 
-        // Create followed users Posts ref
-        const followedUserPostsRef = admin
-                 .firestore()
-                 .collection('posts')
-                 .doc(userId)
-                 .collection('userPosts');
+    // Create followed users Posts ref
+    const followedUserPostsRef = admin
+      .firestore()
+      .collection("posts")
+      .doc(userId)
+      .collection("userPosts");
 
-        // Create following user's Timeline ref
-        const timelinePostsRef = admin
-                .firestore()
-                .collection('timeline')
-                .doc(followerId)
-                .collection('timelinePosts')
+    // Create following user's Timeline ref
+    const timelinePostsRef = admin
+      .firestore()
+      .collection("timeline")
+      .doc(followerId)
+      .collection("timelinePosts");
 
-        // Get the followed users post
-        const querySnapshot = await followedUserPostsRef.get();
+    // Get the followed users post
+    const querySnapshot = await followedUserPostsRef.get();
 
-        // 4 Add each users post to following user's timeline
-        querySnapshot.forEach(doc => {
-            if(doc.exists){
-                const postId = doc.id;
-                doc.data();
-                timelinePostsRef.doc(postId).set(postData);
-            }
+    // 4 Add each users post to following user's timeline
+    querySnapshot.forEach(doc => {
+      if (doc.exists) {
+        const postId = doc.id;
+        const postData = doc.data();
+        timelinePostsRef.doc(postId).set(postData);
+      }
+    });
+  });
+
+exports.onDeleteFollower = functions.firestore
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onDelete(async (snapshot, context) => {
+    console.log("Follower Deleted", snapshot.id);
+
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    // Create following user's Timeline ref
+    const timelinePostsRef = admin
+      .firestore()
+      .collection("timeline")
+      .doc(followerId)
+      .collection("timelinePosts")
+      .where("ownerId", "==", userId);
+
+    const querySnapshot = await timelinePostsRef.get();
+
+    querySnapshot.forEach(doc => {
+      if (doc.exists) {
+        doc.ref.delete();
+      }
+    });
+  });
+
+//When a post is created we wanr to add a post of each followers of post owner
+
+exports.onCreatePost = functions.firestore
+  .document("/posts/{userId}/userPosts/{postId}")
+  .onCreate(async (snapshot, context) => {
+    const postCreated = snapshot.data();
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+
+    // get all the followers of the user who made the post
+    const userFollowersRef = admin
+      .firestore()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+
+    const querySnapshot = await userFollowersRef.get();
+
+    // 2) add new post to each followers timeline
+
+    querySnapshot.forEach(doc => {
+      const followerId = doc.id;
+
+      admin
+        .firestore()
+        .collection("timeline")
+        .doc(followerId)
+        .collection("timelinePosts")
+        .doc(postId)
+        .set(postCreated);
+    });
+  });
+
+exports.onUpdatePost = functions.firestore
+  .document("/posts/{userId}/userPosts/{postId}")
+  .onUpdate(async (change, context) => {
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+    const postUpdated = change.after.data();
+
+    const userFollowersRef = admin
+      .firestore()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+
+    const querySnapshot = await userFollowersRef.get();
+
+    // 2) Update each post in each followers timeline
+
+    querySnapshot.forEach(doc => {
+      const followerId = doc.id;
+
+      admin
+        .firestore()
+        .collection("timeline")
+        .doc(followerId)
+        .collection("timelinePosts")
+        .doc(postId)
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            doc.ref.update(postUpdated);
+          }
+        });
+    });
+  });
+
+exports.onDeletePost = functions.firestore
+  .document("/posts/{userId}/userPosts/{postId}")
+  .onDelete(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+
+    const userFollowersRef = admin
+      .firestore()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+
+    const querySnapshot = await userFollowersRef.get();
+
+    // 2) Delete each post in each followers timeline
+
+    querySnapshot.forEach(doc => {
+      const followerId = doc.id;
+
+      admin
+        .firestore()
+        .collection("timeline")
+        .doc(followerId)
+        .collection("timelinePosts")
+        .doc(postId)
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            doc.ref.delete();
+          }
+        });
+    });
+  });
+
+exports.onCreateActivityFeedItem = functions.firestore
+  .document("/feed/{userId}/feedItems/{activityFeedItem}")
+  .onCreate(async (snapshot, context) => {
+    console.log("Activity Feed Item created", snapshot.data());
+
+    // 1) Get user connected to the feed
+
+    const userId = context.params.userId;
+
+    const userRef = admin.firestore().doc(`users/${userId}`);
+
+    const doc = await userRef.get();
+
+    // 2) Once we Have User check if they Have a notification Token;
+    // Send Notification if they have a token
+    const androidNotificationToken = doc.data().androidNotificationToken;
+    const createdActivityFeedItem = snapshot.data();
+
+    if (androidNotificationToken) {
+      sendNotification(androidNotificationToken, createdActivityFeedItem);
+    } else {
+      console.log("No Token for user, cannot send Notification");
+    }
+
+    function sendNotification(androidNotificationToken, activityFeedItem) {
+      let body;
+      // 4) Switch Body Value based on Notifiaction Type
+      switch (activityFeedItem.type) {
+        case "comment":
+          body = `${activityFeedItem.username} à répondu ${activityFeedItem.commentData}`;
+          break;
+
+        case "like":
+          body = `${activityFeedItem.username} à aimer votre post`;
+          break;
+
+        case "follow":
+          body = `${activityFeedItem.username} à commencer à vous suivre`;
+          break;
+
+        default:
+          break;
+      }
+      // 4) Create message for push Notification
+
+      const message = {
+        notification: { body },
+        token: androidNotificationToken,
+        data: { recipient: userId }
+      };
+
+      // 5) Send message with admin messaging()
+
+      admin
+        .messaging()
+        .send(message)
+        .then(response => {
+          // Response is a message ID String
+          console.log("Successfully sent message", response);
         })
-     })
+        .catch(error => {
+          console.log("Error Sending message", error);
+        });
+    }
+  });
